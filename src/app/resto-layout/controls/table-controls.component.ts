44b0ebcb74d731099ee5f0ed953eeaa1cc6c8bd5
Jason <jason.cooke@hotmail.fr>
2022-05-23 11:57:17 +0200

    Refactor table resizing, removed bootstrap from dependencies

--- src/app/resto-layout/controls/table-controls.component.ts
+++ src/app/resto-layout/controls/table-controls.component.ts
@@ -28,28 +28,73 @@ export class TableControlsComponent implements OnInit {
   }
 
   incrementProperty(value: string) {
-    let table = this.table;
-    if ((table[value] + this.incrementValue) <= this.valueMin &&
-      // @ts-ignore
-      (this.table[value] + this.incrementValue) >= this.valueMax) {
-      // @ts-ignore
-      this.interval = setInterval(() => this.table[value] += this.incrementValue, 100)
+    if (this.checkPropertyChange(value, true)) {
+      if (value === 'width') {
+        this.interval = setInterval(() =>
+          this.checkPropertyChange(value, true) ?
+          this.table.width += this.incrementValue : this.stopInterval(), 100)
+      } else if (value === 'height') {
+        this.interval = setInterval(() =>
+          this.checkPropertyChange(value, true) ?
+          this.table.height += this.incrementValue : this.stopInterval(), 100)
+      }
     }
   }
 
   reduceProperty(value: string) {
-    // @ts-ignore
-    this.interval = setInterval(() => this.table[value] -= this.incrementValue, 100)
+    if (this.checkPropertyChange(value, true)) {
+      if (value === 'width') {
+
+        this.interval = setInterval(() =>
+          this.checkPropertyChange(value, false) ?
+            this.table.width -= this.incrementValue :
+            this.stopInterval(), 100)
+
+      } else if (value === 'height') {
+
+        this.interval = setInterval(() =>
+          this.checkPropertyChange(value, false) ?
+            this.table.height -= this.incrementValue :
+            this.stopInterval(), 100)
+
+      }
+    }
   }
 
   incrementPropertyOnce(value: string) {
-    // @ts-ignore
-    this.table[value] += this.incrementValue;
+    if (this.checkPropertyChange(value, true)) {
+      if (value === 'width') {
+        this.table.width += this.incrementValue;
+      } else if (value === 'height') {
+        this.table.height += this.incrementValue;
+      }
+    }
   }
 
   reducePropertyOnce(value: string) {
-    // @ts-ignore
-    this.table[value] -= this.incrementValue;
+    if (this.checkPropertyChange(value, false)) {
+      if (value === 'width') {
+        this.table.width -= this.incrementValue;
+      } else if (value === 'height') {
+        this.table.height -= this.incrementValue;
+      }
+    }
+  }
+
+  checkPropertyChange(value: string, increment: boolean) {
+    if (value === 'width') {
+
+      let newWidth = increment ? this.table.width + this.incrementValue : this.table.width - this.incrementValue;
+      return newWidth >= this.valueMin.width && newWidth <= this.valueMax.width;
+
+    } else if (value === 'height') {
+
+      let newHeight = increment ? this.table.height + this.incrementValue : this.table.height - this.incrementValue;
+      return newHeight >= this.valueMin.height && newHeight <= this.valueMax.height;
+
+    }
+
+    return false;
   }
 
   stopInterval() {
