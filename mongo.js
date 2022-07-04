b06528894a52759852957b30a25b41e5b0ee69f0
Jason <jason.cooke@hotmail.fr>
2022-07-04 16:51:01 +0200

    removed mongo.js

--- mongo.js
+++
@@ -1,6 +0,0 @@
-const username = encodeURIComponent('syre');
-const password = encodeURIComponent('N0cns9oDCStUY49h');
-const cluster = "cluster0.zb4w3";
-const dbName = "Resto";
-
-module.exports = {username, password, cluster, dbName};
