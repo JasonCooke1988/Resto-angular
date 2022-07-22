3e2c519e04287550b63647f10d56ce894edfe7f8
Jason <jason.cooke@hotmail.fr>
2022-07-22 15:41:53 +0200

    modified layout controls

--- app/src/app/components/button/button.component.ts
+++ app/src/app/components/button/button.component.ts
@@ -2,18 +2,14 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  template:
-    `
-      <button
-        (click)="toggleMouseDown()"
-        [ngClass]="[buttonState === 'validate' ? 'bg-denim hover:bg-denim/80' : '', buttonState === 'alert' ? 'bg-red-700 hover:bg-red-700/80' : '']"
-        class="font-small uppercase text-white py-2 px-4 rounded transition ease duration-500 cursor:pointer">
-        {{ buttonText }}</button>`,
+  templateUrl: 'button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
-  @Input() buttonText!: string;
+  @Input() buttonText: string = '';
   @Input() buttonState: string = 'validate';
+  @Input() buttonDisabled: boolean = false;
+  @Input() icon: string = '';
   mouseDown: string = 'false';
 
   constructor() {
