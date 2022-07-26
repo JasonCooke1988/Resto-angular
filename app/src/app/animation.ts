ec66611e000fe19b20c8b6cf431ac83f084c1820
Jason <jason.cooke@hotmail.fr>
2022-07-26 11:22:38 +0200

    Add Client => Home & Home => Client animation

--- app/src/app/animation.ts
+++ app/src/app/animation.ts
@@ -79,49 +79,85 @@ export const slideInAnimation =
             optional: true
           }),
       ]),
-    ])
-    // transition('* <=> *', [
-    //   style({position: 'relative'}),
-    //   query(':enter, :leave', [
-    //       style({
-    //         position: 'absolute',
-    //         top: 0,
-    //         right: 0,
-    //         width: '100%'
-    //       })
-    //     ],
-    //     {
-    //       optional: true
-    //     }),
-    //   query(':enter', [
-    //       style({right: '-100%'})
-    //     ],
-    //     {
-    //       optional: true
-    //     }),
-    //   query(':leave', animateChild(),
-    //     {
-    //       optional: true
-    //     }),
-    //   group([
-    //     query(':leave', [
-    //         animate('300ms ease-out', style({right: '100%', opacity: 0}))
-    //       ],
-    //       {
-    //         optional: true
-    //       }),
-    //     query(':enter', [
-    //         animate('300ms ease-out', style({right: '0%'}))
-    //       ],
-    //       {
-    //         optional: true
-    //       }),
-    //     query('@*', animateChild(),
-    //       {
-    //         optional: true
-    //       })
-    //   ]),
-    // ])
+    ]),
+    transition('Client => Home', [
+      style({position: 'relative'}),
+      query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%'
+          })
+        ],
+        {
+          optional: true
+        }
+      ),
+      query(':enter', [
+          style({left: '-100%'})
+        ],
+        {
+          optional: true
+        }),
+      query(':leave', animateChild(),
+        {
+          optional: true
+        }),
+      group([
+        query(':leave', [
+            animate('300ms ease-out', style({left: '100%'}))
+          ],
+          {
+            optional: true
+          }),
+        query(':enter', [
+            animate('300ms ease-out', style({left: '0%'}))
+          ],
+          {
+            optional: true
+          }),
+      ]),
+    ]),
+    transition('Home => Client', [
+      style({position: 'relative'}),
+      query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            width: '100%'
+          })
+        ],
+        {
+          optional: true
+        }
+      ),
+      query(':enter', [
+          style({right: '-100%'})
+        ],
+        {
+          optional: true
+        }),
+      query(':leave', animateChild(),
+        {
+          optional: true
+        }),
+      group([
+        query(':leave', [
+            animate('300ms ease-out', style({right: '100%'}))
+          ],
+          {
+            optional: true
+          }),
+        query(':enter', [
+            animate('300ms ease-out', style({right: '0%'}))
+          ],
+          {
+            optional: true
+          }),
+      ]),
+    ]),
   ]);
 
 export const mouseState =
